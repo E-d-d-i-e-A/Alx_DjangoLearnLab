a78ec7c210f9a238c1698ab37829f9bfaf925a0a
Edidiong Aquatang <eaquatang@gmail.com>
2025-11-10 04:16:34 +0900

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,24 +1,9 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian, UserProfile
 
 # Register your models here.
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['name']
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
-    list_filter = ['author']
-
-
-@admin.register(Library)
-class LibraryAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    filter_horizontal = ['books']
-
-
-@admin.register(Librarian)
-class LibrarianAdmin(admin.ModelAdmin):
-    list_display = ['name', 'library']
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+admin.site.register(UserProfile)
