2d3a5fdd068a4d3885b0b31138d39b62c8e2049c
Edidiong Aquatang <eaquatang@gmail.com>
2025-11-16 09:01:02 +0100

    Implement CustomUser model and Book permissions
    Added custom user model and manager with additional fields and permissions for the Book model.

--- advanced_features_and_security/LibraryProject/manage.py
+++ advanced_features_and_security/LibraryProject/manage.py
@@ -1,1 +1,67 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom manager for CustomUser model.
+    """
+    def create_user(self, username, email=None, password=None, date_of_birth=None, **extra_fields):
+        """
+        Create and save a regular user with the given username, email, password, and date_of_birth.
+        """
+        if not username:
+            raise ValueError('The Username field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email=None, password=None, date_of_birth=None, **extra_fields):
+        """
+        Create and save a superuser with the given username, email, password, and date_of_birth.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(username, email, password, date_of_birth, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends AbstractUser with additional fields.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username
+
+
+class Book(models.Model):
+    """
+    Book model with custom permissions for view, create, edit, and delete operations.
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view book'),
+            ('can_create', 'Can create book'),
+            ('can_edit', 'Can edit book'),
+            ('can_delete', 'Can delete book'),
+        ]
+    
+    def __str__(self):
+        return self.title
