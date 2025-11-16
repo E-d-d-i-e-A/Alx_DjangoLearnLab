db75592d561b2b3604996056c051172f372d462c
Edidiong Aquatang <eaquatang@gmail.com>
2025-11-16 09:07:49 +0100

    Modify URL patterns for admin and media file
    Updated URL patterns to include admin and static file serving.

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
-    path('books/create/', views.book_create, name='book_create'),
-    path('books/<int:pk>/edit/', views.book_edit, name='book_edit'),
-    path('books/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('admin/', admin.site.urls),
+    path('', include('bookshelf.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
