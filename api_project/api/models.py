4ea0f443dca88c44ac8cc2f2b4a8b8801f86e47b
Edidiong Aquatang <eaquatang@gmail.com>
2025-11-23 07:34:22 +0100

    Create models.py

---
+++ api_project/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    """
+    Book model for the API.
+    Represents a book with title and author information.
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
