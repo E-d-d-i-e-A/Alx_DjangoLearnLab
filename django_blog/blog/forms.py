5421fab2e7025a7752447ee58b44f02ea4ff3c4c
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-02 21:44:05 +0100

    Implement custom user forms with email support
    Added custom user creation and update forms with email field.

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    Extended user registration form with email field.
+    
+    Extends Django's built-in UserCreationForm to include email field
+    as a required field during registration.
+    
+    Fields:
+        username: Username for login
+        email: Email address (required)
+        password1: Password
+        password2: Password confirmation
+    """
+    email = forms.EmailField(
+        required=True,
+        help_text='Required. Enter a valid email address.',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+    )
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
+    
+    def save(self, commit=True):
+        """
+        Save the user with email.
+        
+        Ensures the email is saved to the user model.
+        """
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Form for updating user profile information.
+    
+    Allows users to update their username and email address.
+    """
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+    )
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+        }
