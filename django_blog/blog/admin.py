d9283f50cff05a14f999cb75882def503455bcfb
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-06 07:11:11 +0100

    Update admin.py

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -13,3 +13,16 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['published_date', 'author']
     search_fields = ['title', 'content']
     date_hierarchy = 'published_date'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Comment model.
+    
+    Customizes the admin interface for managing comments.
+    """
+    list_display = ['author', 'post', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at', 'author']
+    search_fields = ['content', 'author__username', 'post__title']
+    date_hierarchy = 'created_at'
