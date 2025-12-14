46f79b4e0b62d7c468252cb229b1f3a178616aa9
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-14 15:31:12 +0100

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,18 +3,14 @@ from .views import (
     UserRegistrationView, 
     UserLoginView, 
     UserProfileView,
-    follow_user,
-    unfollow_user,
-    list_followers,
-    list_following
+    FollowUserView,
+    UnfollowUserView
 )
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow/<int:user_id>/', follow_user, name='follow-user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
-    path('followers/', list_followers, name='list-followers'),
-    path('following/', list_following, name='list-following'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
