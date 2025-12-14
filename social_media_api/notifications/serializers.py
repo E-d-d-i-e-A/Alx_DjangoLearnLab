28f2394692cd76e22caf2ee928bed36012903f7c
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-14 15:42:22 +0100

    Create notifications/serializers.py

---
+++ social_media_api/notifications/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    actor_username = serializers.CharField(source='actor.username', read_only=True)
+    recipient_username = serializers.CharField(source='recipient.username', read_only=True)
+    
+    class Meta:
+        model = Notification
+        fields = ['id', 'recipient', 'recipient_username', 'actor', 'actor_username', 
+                  'verb', 'target_content_type', 'target_object_id', 'timestamp', 'read']
+        read_only_fields = ['id', 'recipient', 'actor', 'timestamp']
