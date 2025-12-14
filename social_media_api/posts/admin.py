126b0b1ee29d51ada92af1d23e12b25418d95a4b
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-14 14:56:14 +0100

    Create posts/admin.py

---
+++ social_media_api/posts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at', 'author']
+    search_fields = ['title', 'content', 'author__username']
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at', 'author']
+    search_fields = ['content', 'author__username', 'post__title']
+    date_hierarchy = 'created_at'
