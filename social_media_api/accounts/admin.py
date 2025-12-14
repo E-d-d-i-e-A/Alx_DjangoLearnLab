47b18762b048be335870a33306c89337c3c514ef
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-14 11:30:41 +0100

    Create accounts/admin.py

---
+++ social_media_api/accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'profile_picture', 'followers')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('bio', 'profile_picture')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
