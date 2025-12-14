192960c240ad960cbc8dd62fa166a55cba39341f
Edidiong Aquatang <eaquatang@gmail.com>
2025-12-14 15:22:50 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth import get_user_model
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+
+User = get_user_model()
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
@@ -42,3 +47,27 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def user_feed(request):
+    """
+    Get a feed of posts from users that the current user follows.
+    Returns posts ordered by creation date (most recent first).
+    """
+    # Get users that the current user is following
+    following_users = request.user.following.all()
+    
+    # Get posts from followed users, ordered by creation date (newest first)
+    feed_posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+    
+    # Paginate the results
+    paginator = StandardResultsSetPagination()
+    paginated_posts = paginator.paginate_queryset(feed_posts, request)
+    
+    # Serialize the posts
+    serializer = PostSerializer(paginated_posts, many=True)
+    
+    # Return paginated response
+    return paginator.get_paginated_response(serializer.data)
